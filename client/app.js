d65cd6914179f8f41844e536375812c15bacb1e0
Scott Robertson <scott@arishill.com>
2015-01-19 23:06:24 -0800

    use helpers/events from packages (pass template as its the only way i know how to do it at the moment)

--- client/app.js
+++ client/app.js
@@ -1,25 +1,11 @@
 App = {};
 
-Template.registerHelper('withIndex', function (all) {
-  return _.map(all, function(val, index) {
-      return {index: index, value: val};
-  });
-});
-
-Template.registerHelper('timestampToDate', function(date) {
-  if (_.isDate(date)) {
-    return new Date(date);
-  }
-});
-
-Template.registerHelper('centsToDollars', function(cents) {
-    if (_.isNumber(cents)) {
-      return parseFloat(cents / 100).toFixed(2);
-    }
-});
-
+GlobalTemplates.useHelpers(Template);
 ProductsTemplates.useHelpers(Template);
 ProductsTemplates.useEvents(Template);
 ProductsTemplates.useCallbacks(Template);
 
-
+Imgix.configSource({
+  name: '***REMOVED***',
+  token: '***REMOVED***'
+});
