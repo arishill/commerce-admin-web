059488016eab838d63ea0ffbf5c3c619285e0a45
Scott Robertson <scott@arishill.com>
2015-02-17 21:59:43 -0800

    add transactions/brainstree config/setup

--- server/config.js
+++ server/config.js
@@ -3,3 +3,23 @@ Uploader.config({
   email: Meteor.settings.gcloud.email,
   bucket: Meteor.settings.gcloud.bucket
 });
+
+Transactions.setup({
+  default_gateway: 'braintree',
+  backup_gateway: 'stripe',
+  store_card: false,
+  authorize_only: true,
+
+  // braintree
+  braintree: {
+    channel:      '***REMOVED***',
+    environment:  'sandbox',
+    merchant_id:  '***REMOVED***',
+    public_key:   '***REMOVED***',
+    private_key:  '***REMOVED***'
+  },
+  // stripe
+  stripe: {
+
+  }
+});
