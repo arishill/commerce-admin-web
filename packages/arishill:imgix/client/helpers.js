b04c3ddd6a39c8e41a73ff35db5ce423b09a4727
Scott Robertson <scott@arishill.com>
2015-02-17 22:00:26 -0800

    fixes for imgix helpers

--- packages/arishill:imgix/client/helpers.js
+++ packages/arishill:imgix/client/helpers.js
@@ -2,13 +2,10 @@
 
 /* IMGIX HELPERS
 .................................................*/
+Template.registerHelper('imgix', function(url, definition) {
+  var definition = Imgix.definitions[definition];
 
-ImgixHelpers = {
-  'imgix': function(url, definition) {
-    var definition = Imgix.definitions[definition];
+  image = new imgix.URL('https://' + Imgix.source.name + '.imgix.net/' + url, definition, Imgix.source.token);
 
-    image = new imgix.URL('https://' + Imgix.source.name + '.imgix.net/' + url, definition, Imgix.source.token);
-
-    return image.getUrl();
-  }
-};
+  return image.getUrl();
+});
