1e98188bb3a0886fd44dd8b59ad7960e3174493e
Scott Robertson <scott@arishill.com>
2015-02-17 21:58:33 -0800

    fixes for client package

--- packages/arishill:client/client/helpers.js
+++ packages/arishill:client/client/helpers.js
@@ -1,25 +1,32 @@
-// definitions
-GlobalTemplates = (_.isUndefined(GlobalTemplates)) ? {} : GlobalTemplates;
 
-/* GLOBAL HELPERS
-.................................................*/
-
-GlobalTemplates.useHelpers = function(Template) {
-  Template.registerHelper('withIndex', function (all) {
-    return _.map(all, function(val, index) {
-        return {index: index, value: val};
-    });
+Template.registerHelper('withIndex', function (all) {
+  return _.map(all, function(val, index) {
+      return {index: index, value: val};
   });
+});
 
-  Template.registerHelper('timestampToDate', function(date) {
-    if (_.isDate(date)) {
-      return new Date(date);
-    }
-  });
+Template.registerHelper('timestampToDate', function(date) {
+  if (_.isDate(date)) {
+    return new Date(date);
+  }
+});
 
-  Template.registerHelper('centsToDollars', function(cents) {
-      if (_.isNumber(cents)) {
-        return parseFloat(cents / 100).toFixed(2);
-      }
-  });
-};
+Template.registerHelper('centsToDollarsLabel', function(cents) {
+  var symbol = '$';
+
+  cents = _.isNumber(cents) ? cents: 0;
+
+  return symbol + parseFloat(cents / 100).toFixed(2);
+});
+
+Template.registerHelper('centsToDollars', function(cents, with_symbol) {
+  var symbol = '';
+
+  if (_.isBoolean(with_symbol) && with_symbol) {
+    symbol = '$';
+  }
+
+  if (_.isNumber(cents)) {
+    return symbol + parseFloat(cents / 100).toFixed(2);
+  }
+});
