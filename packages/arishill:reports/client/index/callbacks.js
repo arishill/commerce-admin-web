24a7c3248248819a99169fcf84e10091eac6f62e
Scott Robertson <scott@arishill.com>
2015-04-26 11:21:00 +0000

    reportrs package (1st pass)

---
+++ packages/arishill:reports/client/index/callbacks.js
@@ -0,0 +1,5 @@
+/* ARISTOTLE DASHBOARD CALLBACKS
+.................................................*/
+Template.aristotle__reports__index.created = function() {};
+Template.aristotle__reports__index.rendered = function() {};
+Template.aristotle__reports__index.destroyed = function() {};
