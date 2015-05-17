18bddfab0493c87e6f9e639d8cbbe854b2d2cfce
Scott Robertson <scott@arishill.com>
2015-05-17 11:34:00 +0000

    sloppy beginning to validate card package

--- packages/arishill:validate-card/client/helpers.js
+++ packages/arishill:validate-card/client/helpers.js
@@ -5,5 +5,8 @@ Forms.card.helpers = {
   cardType: function() {
     Forms.card.tracker.depend();
     return Forms.card.current.type;
+  },
+  countries: function() {
+    return Forms.card.countries;
   }
 };
