ff4ee8b752ddf16995bf752c8c462054af47b9ca
Scott Robertson <scott@arishill.com>
2015-06-21 14:26:00 +0000

    client package updates

--- packages/arishill:client/client/helpers.js
+++ packages/arishill:client/client/helpers.js
@@ -1,14 +1,53 @@
 
-Template.registerHelper('withIndex', function (all) {
-  return _.map(all, function(val, index) {
-      return {index: index, value: val};
-  });
+Template.registerHelper('withIndex', function (list) {
+  var data = this;
+
+  if (list) {
+    if (_.isArray(list)) {
+      return _.map(list, function (value, i) {
+          value.index = i;
+          value.data = data;
+          return value;
+      });
+    } else if (_.isObject(list.collection)) {
+      return list.map(function(document, index){
+        document.index = index;
+        document.data = data;
+        return document;
+      });
+    }
+  }
 });
 
-Template.registerHelper('timestampToDate', function(date) {
-  if (_.isDate(date)) {
-    return new Date(date);
+Template.registerHelper('withParent', function (list) {
+  if (list) {
+    if (_.isArray(list)) {
+      return _.map(list, function (value, i) {
+          value.index = i;
+          return value;
+      });
+    } else if (_.isObject(list.collection)) {
+      return list.map(function(document, index){
+        document.index = index;
+        return document;
+      });
+    }
   }
+});
+
+Template.registerHelper('timestampToDate', function(timestamp, format) {
+  var day = moment(timestamp);
+  return day.format(format);
+});
+
+Template.registerHelper('getDate', function(name, format) {
+  switch (name) {
+    case 'today':
+      var timestamp = Date.now();
+      var day = moment(timestamp);
+    break;
+  }
+  return day.format(format);
 });
 
 Template.registerHelper('centsToDollarsLabel', function(cents) {
@@ -29,4 +68,70 @@ Template.registerHelper('centsToDollars', function(cents, with_symbol) {
   if (_.isNumber(cents)) {
     return symbol + parseFloat(cents / 100).toFixed(2);
   }
+});
+
+Template.registerHelper('checkbox', function(name, id, index, checked, size) {
+  var checked_attr = index_attr = size_attr = '';
+
+  if (typeof(checked) !== 'undefined' && checked) {
+    checked_attr = 'checked';
+  }
+  if (typeof(size) !== 'undefined' && size) {
+    size_attr = 'checkbox--' + size;
+  }
+  if (typeof(index) !== 'undefined') {
+    index_attr = '[' + index + ']';
+  }
+
+  return '<div class="checkbox ' + size_attr + '">' +
+    '<input type="checkbox" name="' + name + index_attr + '" id="' + id + index_attr + '" ' + checked_attr + '>' +
+    '<label for="' + id + index_attr + '">' +
+      '<i class="icon-check-site"></i>' +
+    '</label>' +
+  '</div>';
+});
+
+Template.registerHelper('checkboxAlt', function(params) {
+  var checked_attr = index_attr = size_attr = '';
+  var defaults = {
+    id: '',
+    name: '',
+    index: null,
+    checked: false
+  }
+
+  params = _.extend({}, defaults, params.hash);
+
+  if (params.checked) {
+    checked_attr = 'checked';
+  }
+  if (params.size) {
+    size_attr = 'checkbox--' + params.size;
+  }
+  if (params.index) {
+    index_attr = '[' + index + ']';
+  }
+
+  return '<div class="checkbox-alt ' + size_attr + '">' +
+    '<input type="checkbox" name="' + params.name + index_attr + '" id="' + params.id + index_attr + '" ' + checked_attr + '>' +
+    '<label for="' + params.id + index_attr + '">' +
+      '<span class="disc"></span>' +
+    '</label>' +
+  '</div>';
+});
+
+Template.registerHelper('isActiveRoute', function (route_name) {
+  if (Iron.Location.get().path.indexOf(route_name) >= 0) {
+    return 'is-active';
+  }
+});
+
+Template.registerHelper('JSONStringify', function (obj) {
+  var json = '';
+
+  try {
+    json = JSON.stringify(obj);
+  } catch (e) {};
+
+  return json;
 });
