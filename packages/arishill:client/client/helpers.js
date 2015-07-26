0f2a2590db157e16e19f36e3cdc1de692d331538
Scott Robertson <scott@arishill.com>
2015-07-26 08:24:00 +0000

    remove checkbox helpers (now components)

--- packages/arishill:client/client/helpers.js
+++ packages/arishill:client/client/helpers.js
@@ -70,58 +70,9 @@ Template.registerHelper('centsToDollars', function(cents, with_symbol) {
   }
 });
 
-Template.registerHelper('checkbox', function(name, id, index, checked, size) {
-  var checked_attr = index_attr = size_attr = '';
-
-  if (typeof(checked) !== 'undefined' && checked) {
-    checked_attr = 'checked';
-  }
-  if (typeof(size) !== 'undefined' && size) {
-    size_attr = 'checkbox--' + size;
-  }
-  if (typeof(index) !== 'undefined') {
-    index_attr = '[' + index + ']';
-  }
-
-  return '<div class="checkbox ' + size_attr + '">' +
-    '<input type="checkbox" name="' + name + index_attr + '" id="' + id + index_attr + '" ' + checked_attr + '>' +
-    '<label for="' + id + index_attr + '">' +
-      '<i class="icon-check-site"></i>' +
-    '</label>' +
-  '</div>';
-});
-
-Template.registerHelper('checkboxAlt', function(params) {
-  var checked_attr = index_attr = size_attr = '';
-  var defaults = {
-    id: '',
-    name: '',
-    index: null,
-    checked: false
-  }
-
-  params = _.extend({}, defaults, params.hash);
-
-  if (params.checked) {
-    checked_attr = 'checked';
-  }
-  if (params.size) {
-    size_attr = 'checkbox--' + params.size;
-  }
-  if (params.index) {
-    index_attr = '[' + index + ']';
-  }
-
-  return '<div class="checkbox-alt ' + size_attr + '">' +
-    '<input type="checkbox" name="' + params.name + index_attr + '" id="' + params.id + index_attr + '" ' + checked_attr + '>' +
-    '<label for="' + params.id + index_attr + '">' +
-      '<span class="disc"></span>' +
-    '</label>' +
-  '</div>';
-});
-
 Template.registerHelper('isActiveRoute', function (route_name) {
-  if (Iron.Location.get().path.indexOf(route_name) >= 0) {
+  var route = route_name.split('_')[0];
+  if (Router.current().location.get().path.indexOf(route) >= 0) {
     return 'is-active';
   }
 });
