9b6916d8c272aa08c64a4464a9ca3c3e7ef8ab8f
Scott Robertson <scott@arishill.com>
2015-08-01 08:59:49 -0700

    product packages refinements (styling)

--- packages/arishill:products/client/helpers.js
+++ packages/arishill:products/client/helpers.js
@@ -17,3 +17,30 @@ Template.aristotle__products__table_body.helpers({
 Template.aristotle__products__form_variants.helpers({
   getProductStock: Helpers.products.getProductStock
 });
+
+
+Template.aristotle__products__table_head.helpers({
+  'getDropdownOptions': function() {
+    return {
+      css: 'dropdown--bottom dropdown--small',
+      anchor: {
+        css: 'icon-filter-gray icon--center',
+        text: 'Filter'
+      },
+      options: {
+        close_on_click: true,
+        select_input: true,
+        name: 'pagination'
+      },
+      list: {
+        css: 'text-center text-small arrow-top-middle box-shadow',
+        items: [
+          { text: 'Popular', value: '1' },
+          { text: 'Featured', value: '2' },
+          { text: 'Men\'s', value: '3' },
+          { text: 'Women\'s', value: '4' }
+        ]
+      }
+    };
+  }
+});
