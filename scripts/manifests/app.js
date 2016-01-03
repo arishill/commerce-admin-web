75acb06aaaef6761cb6df1989ed1c459b4408322
scottusrobus <scott@scottusrobus.com>
2016-01-03 10:57:30 -0800

    update manifests

--- scripts/manifests/app.js
+++ scripts/manifests/app.js
@@ -1,19 +1,55 @@
 'use strict';
 
 let Manifest = [
+  'client/ext.js',
   'client/scope.js',
+
+  /* shared components */
+  'client/views/_shared/header/view.js',
+
   /* primary nav component */
-  'client/navigation/primary/helpers.js',
-  'client/navigation/primary/state.js',
-  'client/navigation/primary/view.js',
+  'client/views/navigation/primary/helpers.js',
+  'client/views/navigation/primary/state.js',
+  'client/views/navigation/primary/view.js',
 
   /* secondary nav component */
-  'client/navigation/secondary/helpers.js',
-  'client/navigation/secondary/state.js',
-  'client/navigation/secondary/view.js',
+  'client/views/navigation/secondary/helpers.js',
+  'client/views/navigation/secondary/state.js',
+  'client/views/navigation/secondary/view.js',
+
+  /* content > dashboard component */
+  'client/views/content/dashboard/index/view.js',
+
+  /* shop ..................................... */
+
+    /* dashboard components */
+    'client/views/shop/dashboard/index/view.js',
+
+    /* products components */
+    'client/views/shop/products/index/view.js',
+    'client/views/shop/products/show/view.js',
+
+    /* orders components */
+    'client/views/shop/orders/index/view.js',
+    'client/views/shop/orders/show/view.js',
+
+    /* coupons components */
+    'client/views/shop/coupons/index/view.js',
+    'client/views/shop/coupons/show/view.js',
+
+    /* sales components */
+    'client/views/shop/sales/index/view.js',
+    'client/views/shop/sales/show/view.js',
+
+    /* categories components */
+    'client/views/shop/categories/index/view.js',
+    'client/views/shop/categories/show/view.js',
+
+  /* settings > dashboard component */
+  'client/views/settings/dashboard/index/view.js',
 
   /* layouts */
-  'client/layouts/default/view.js',
+  'client/views/layouts/default/view.js',
 
   /* routes */
   'client/routes.js'
--- scripts/manifests/vendor.js
+++ scripts/manifests/vendor.js
@@ -2,7 +2,8 @@
 
 let Manifest = [
   'node_modules/underscore/underscore.js',
-  'node_modules/mithril/mithril.js'
+  'node_modules/mithril/mithril.js',
+  'node_modules/arishill-mithril/distr/arishill-mithril.js'
 ];
 
 module.exports = Manifest;
