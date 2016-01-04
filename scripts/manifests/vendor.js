f995cf4ccee4073d42c0eb6e3d0de58c1bdba56b
scottusrobus <scott@scottusrobus.com>
2016-01-03 22:53:45 -0800

    add accounting, money, and moment packages

--- scripts/manifests/vendor.js
+++ scripts/manifests/vendor.js
@@ -2,6 +2,8 @@
 
 let Manifest = [
   'node_modules/underscore/underscore.js',
+  'node_modules/moment/moment.js',
+  'node_modules/accounting/accounting.js',
   'node_modules/mithril/mithril.js',
   'node_modules/arishill-mithril/distr/arishill-mithril.js'
 ];
