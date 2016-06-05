3e8c641d50b55e555a5994653581a2ed95360553
scottusrobus <scott@scottusrobus.com>
2016-06-05 13:43:23 -0700

    add util methods for add/removing class

--- client/_core/ext.js
+++ client/_core/ext.js
@@ -1,1 +1,28 @@
 'use strict';
+
+_.hasClass = function(el, className) {
+  if (el.classList) {
+    return el.classList.contains(className);
+  }
+  else {
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+  }
+};
+
+_.addClass = function(el, className) {
+  if (el.classList) {
+    el.classList.add(className);
+  } else if (!_.hasClass(el, className)) {
+    el.className += " " + className;
+  }
+};
+
+_.removeClass = function(el, className) {
+  if (el.classList) {
+    el.classList.remove(className);
+  }
+  else if (_.hasClass(el, className)) {
+    let reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+    el.className=el.className.replace(reg, ' ');
+  }
+};
